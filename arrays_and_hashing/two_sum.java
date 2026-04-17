583cd16d8e511f142a99514ae2d97a5242737d0e
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-04-18 00:18:03 +0530

    Create two_sum.java

---
+++ arrays_and_hashing/two_sum.java
@@ -0,0 +1,20 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i=0; i < nums.length;i++){
+            if (!map.containsKey(target-nums[i])){
+                map.put(nums[i],i);
+            }
+            else return new int[]{i,map.get(target-nums[i])};
+        }
+        return new int[]{-1,-1};
+    }
+}
