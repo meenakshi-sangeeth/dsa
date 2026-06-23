8da1e830311f2e9d011346cda3f46cc23f5190b7
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-06-23 23:47:15 +0530

    Create search_a_2d_matrix.java

---
+++ binary_search/search_a_2d_matrix.java
@@ -0,0 +1,42 @@
+/*
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+ 
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+*/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int low=0;
+        int high=m*n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=mid/n;
+            int col=mid%n;
+            if(matrix[row][col]==target) return true;
+            else if (matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return false;
+    }
+}
