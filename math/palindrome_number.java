8c57cf9b3a48710de57347f16920d7dca02ed9cd
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-06-28 23:07:09 +0530

    Update palindrome_number.java

--- math/palindrome_number.java
+++ math/palindrome_number.java
@@ -1,3 +1,17 @@
 /*
 Given an integer x, return true if x is a palindrome, and false otherwise.
 */
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int original=x;
+        int reverse=0;
+        while (x != 0) {
+            reverse = reverse*10+x%10;
+            x =x/10;
+        }
+        return original==reverse;
+    }
+}
+    
