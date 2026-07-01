1fcca6eb26f9017cd4f6eb950f388ecc01eb6b70
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-07-01 23:34:04 +0530

    Update merge_k_sorted_lists.java

--- linked_list/merge_k_sorted_lists.java
+++ linked_list/merge_k_sorted_lists.java
@@ -24,4 +24,50 @@ Input: lists = [[]]
 Output: []
 */
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists==null || lists.length==0)
+            return null;
+        int interval=1;
+        while (interval<lists.length) {
+            for (int i=0; i+interval < lists.length;i=i+interval*2) {
+                lists[i] = merge(lists[i], lists[i+interval]);
+            }
+            interval=interval*2;
+        }
+        return lists[0];
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0);
+        ListNode curr=dummy;
+        while (l1!=null && l2!=null) {
+            if (l1.val<=l2.val) {
+                curr.next=l1;
+                l1=l1.next;
+            } 
+            else{
+                curr.next=l2;
+                l2=l2.next;
+            }
+
+            curr = curr.next;
+        }
+        if(l1==null) curr.next=l2;
+        else curr.next=l1;
+
+        return dummy.next;
+    }
+    
+}
 
