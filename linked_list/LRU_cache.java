cf988455df83006f56882b4e8c05ee58a35e971c
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-07-01 23:39:17 +0530

    Update LRU_cache.java

--- linked_list/LRU_cache.java
+++ linked_list/LRU_cache.java
@@ -27,3 +27,95 @@ lRUCache.get(1);    // return -1 (not found)
 lRUCache.get(3);    // return 3
 lRUCache.get(4);    // return 4
 */
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
+class LRUCache {
+
+    class Node{
+        int key,val;
+        Node prev,next;
+
+        Node(int key,int val){
+            this.key=key;
+            this.val=val;
+        }
+    }
+
+    int capacity;
+    HashMap<Integer,Node> map;
+    Node head,tail;
+
+    public LRUCache(int capacity){
+
+        this.capacity=capacity;
+        map=new HashMap<>();
+
+        head=new Node(0,0);
+        tail=new Node(0,0);
+
+        head.next=tail;
+        tail.prev=head;
+    }
+
+    private void remove(Node node){
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
+    }
+
+    private void insert(Node node){
+        Node last=tail.prev;
+
+        last.next=node;
+        node.prev=last;
+
+        node.next=tail;
+        tail.prev=node;
+    }
+
+    public int get(int key){
+
+        if(!map.containsKey(key))
+            return -1;
+
+        Node node=map.get(key);
+
+        remove(node);
+        insert(node);
+
+        return node.val;
+    }
+
+    public void put(int key,int value){
+
+        if(map.containsKey(key)){
+            Node node=map.get(key);
+
+            node.val=value;
+
+            remove(node);
+            insert(node);
+
+            return;
+        }
+
+        Node node=new Node(key,value);
+
+        map.put(key,node);
+        insert(node);
+
+        if(map.size()>capacity){
+
+            Node lru=head.next;
+
+            remove(lru);
+            map.remove(lru.key);
+        }
+    }
+}
+
