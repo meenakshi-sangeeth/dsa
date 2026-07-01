cadbb4c65f8746b9d6664c5c5ef8aebabeff6596
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-07-01 23:43:45 +0530

    Update remove_duplicates_from_sorted_array.java

--- arrays_and_hashing/remove_duplicates_from_sorted_array.java
+++ arrays_and_hashing/remove_duplicates_from_sorted_array.java
@@ -32,3 +32,18 @@ Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 */
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0)
+            return 0;
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
