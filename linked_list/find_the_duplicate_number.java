b2c272b9324ade9cdb54bfedd2ecde8e3f621739
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-07-01 23:40:39 +0530

    Update find_the_duplicate_number.java

--- linked_list/find_the_duplicate_number.java
+++ linked_list/find_the_duplicate_number.java
@@ -17,3 +17,20 @@ Example 3:
 Input: nums = [3,3,3,3,3]
 Output: 3
 */
+
+class Solution {
+    public int findDuplicate(int[] nums){
+        int slow=0;
+        int fast=0;
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        slow=0;
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+}
