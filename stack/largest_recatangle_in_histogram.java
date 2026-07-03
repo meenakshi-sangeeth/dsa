78d1c8c1976d6f8fdf56b1c1f58cea5a1283f95a
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-07-03 23:16:49 +0530

    Update largest_recatangle_in_histogram.java

--- stack/largest_recatangle_in_histogram.java
+++ stack/largest_recatangle_in_histogram.java
@@ -11,3 +11,30 @@ Example 2:
 Input: heights = [2,4]
 Output: 4
 */
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack=new Stack<>();
+        int maxArea=0;
+        int n=heights.length;
+
+        for(int i=0;i<=n;i++){
+            int currHeight = (i==n)?0:heights[i];
+            while(!stack.isEmpty() && currHeight<heights[stack.peek()]){
+                int height=heights[stack.pop()];
+                int width;
+
+                if(stack.isEmpty())
+                    width=i;
+                else
+                    width=i-stack.peek()-1;
+
+                maxArea=Math.max(maxArea,height*width);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
