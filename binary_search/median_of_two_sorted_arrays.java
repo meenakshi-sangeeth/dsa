3d5f1f8ecf3ff3e04f71ef7eaa88b1ecc61245ee
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-07-03 23:36:42 +0530

    Update median_of_two_sorted_arrays.java

--- binary_search/median_of_two_sorted_arrays.java
+++ binary_search/median_of_two_sorted_arrays.java
@@ -14,4 +14,48 @@ Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 */
 
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
+        if (nums1.length > nums2.length)
+            return findMedianSortedArrays(nums2, nums1);
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int low = 0;
+        int high = m;
+
+        while (low <= high) {
+
+            int mid1 = (low + high) / 2;
+            int mid2 = (m + n + 1) / 2 - mid1;
+
+            int l1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2 - 1];
+
+            int r1 = (mid1 == m) ? Integer.MAX_VALUE : nums1[mid1];
+            int r2 = (mid2 == n) ? Integer.MAX_VALUE : nums2[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(l1, l2) +
+                            Math.min(r1, r2)) / 2.0;
+                }
+
+                return Math.max(l1, l2);
+            }
+
+            else if (l1 > r2) {
+                high = mid1 - 1;
+            }
+
+            else {
+                low = mid1 + 1;
+            }
+        }
+
+        return 0;
+    }
+}
