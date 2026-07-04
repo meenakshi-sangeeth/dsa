03bc523f6befe966d36707784c700909fce3aa35
meenakshi-sangeeth <meenakshi.sangeeth@gmail.com>
2026-07-04 12:25:44 +0530

    Update maximum_path_sum.java

--- trees/maximum_path_sum.java
+++ trees/maximum_path_sum.java
@@ -15,3 +15,39 @@ Input: root = [-10,9,20,null,null,15,7]
 Output: 42
 Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
 */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max=Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root){
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root){
+        if(root==null)
+            return 0;
+
+        int left=Math.max(0,dfs(root.left));
+        int right=Math.max(0,dfs(root.right));
+
+        max=Math.max(max,left+right+root.val);
+
+        return root.val+Math.max(left,right);
+    }
+}
